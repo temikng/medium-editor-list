c6cc793c7611fada07d3fdbea7bf2b29ac5e2011
Jose Carrillo <josefromsanjose@users.noreply.github.com>
2016-08-08 16:17:26 -0700

    angular: existing list do not get initizlied
    Existing lists do not get initialized when MediumEditorList is used when angular.

--- src/MediumEditorList.js
+++ src/MediumEditorList.js
@@ -14,7 +14,12 @@ var MediumEditorList = MediumEditor.Extension.extend({
         me.editor = this.base;
         me.listInstances = {};
         me.on(me.button, 'click', me.onClick.bind(me));
-        me.initExistsingLists();
+        // and it timeout to send at the end of the event stack. Need when used with angular.
+        (function (self) {
+            window.setTimeout(function () {
+                self.initExistsingLists();
+            }, 0);
+        })(me);
     },
     initExistsingLists: function () {
         var $lists = this.getExistsingLists(),
